a491a9a9669a920b56561125e9dd5af190379392
nguyenphan14 <nguyenph.ptit@gmail.com>
2022-05-07 10:32:04 +0700

    feat: CRUD category

--- src/main/java/com/docongban/controller/admin/AdminController.java
+++ src/main/java/com/docongban/controller/admin/AdminController.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import com.docongban.entity.Category;
+import com.docongban.service.admin.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,71 +33,68 @@ import com.docongban.service.admin.SellerService;
 //@CrossOrigin("http://localhost:8088/admin/**")
 public class AdminController {
 
-	@Autowired
-	OrderAccountRepository orderAccountRepository;
-	
-	@Autowired
-	OrderDetailRepository orderDetailRepository;
-	
-	@Autowired
-	AccountRepository accountRepository;
-	
-	@Autowired
-	AdminAccountService adminAccountService;
-	
-	@Autowired
-	SellerService sellerService;
-	
-	@GetMapping("/bill")
-	public ModelAndView getBill() {
-		ModelAndView modelAndView = new ModelAndView("admin/bill");
-		
-		DecimalFormat df = new DecimalFormat(",###");
-		
-		List<OrderAccount> orderAccounts = orderAccountRepository.findAll();
-		List<OrderDetail> orderDetails = orderDetailRepository.findAll();
-		List<String> totalPrice = new ArrayList<>();
-		
-		for(OrderAccount oc: orderAccounts) {
-			long total=0;
-			for(OrderDetail od: orderDetails) {
-				if(oc.getId()==od.getOrderAccountId()) {
-					total+=od.getProductPrice();
-				}
-			}
-			totalPrice.add(df.format(total));
-		}
-		
-		modelAndView.addObject("orderAccounts", orderAccounts);
-		modelAndView.addObject("orderDetails", orderDetails);
-		modelAndView.addObject("totalPrice", totalPrice);
-		
-		return modelAndView;
-	}
-	
-	@Transactional
-	@GetMapping("/bill/complete/{id}")
-	public RedirectView checkComplete(@PathVariable int id) {
-		orderAccountRepository.updateStatus(id);
-		return new RedirectView("/admin/bill");
-	}
-	
-	
-	/*
-	 * Hiển thị ra danh sách tất cả người dùng (danh sách tất cả account)
-	 * Created by: NPTAN (24/04/2022)
-	 * Version: 1.0
-	 */
-	@GetMapping("/account")
-	public ModelAndView getAccounts() {
-		ModelAndView modelAndView = new ModelAndView("admin/account");
-		List<Account> accounts = adminAccountService.getAllAccounts();
-		modelAndView.addObject("accounts", accounts);
-		return modelAndView;
-	}
-	
-	
-	
-	
-	
+    @Autowired
+    OrderAccountRepository orderAccountRepository;
+
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Autowired
+    AdminAccountService adminAccountService;
+
+    @Autowired
+    SellerService sellerService;
+
+    @GetMapping("/bill")
+    public ModelAndView getBill() {
+        ModelAndView modelAndView = new ModelAndView("admin/bill");
+
+        DecimalFormat df = new DecimalFormat(",###");
+
+        List<OrderAccount> orderAccounts = orderAccountRepository.findAll();
+        List<OrderDetail> orderDetails = orderDetailRepository.findAll();
+        List<String> totalPrice = new ArrayList<>();
+
+        for (OrderAccount oc : orderAccounts) {
+            long total = 0;
+            for (OrderDetail od : orderDetails) {
+                if (oc.getId() == od.getOrderAccountId()) {
+                    total += od.getProductPrice();
+                }
+            }
+            totalPrice.add(df.format(total));
+        }
+
+        modelAndView.addObject("orderAccounts", orderAccounts);
+        modelAndView.addObject("orderDetails", orderDetails);
+        modelAndView.addObject("totalPrice", totalPrice);
+
+        return modelAndView;
+    }
+
+    @Transactional
+    @GetMapping("/bill/complete/{id}")
+    public RedirectView checkComplete(@PathVariable int id) {
+        orderAccountRepository.updateStatus(id);
+        return new RedirectView("/admin/bill");
+    }
+
+
+    /*
+     * Hiển thị ra danh sách tất cả người dùng (danh sách tất cả account)
+     * Created by: NPTAN (24/04/2022)
+     * Version: 1.0
+     */
+    @GetMapping("/account")
+    public ModelAndView getAccounts() {
+        ModelAndView modelAndView = new ModelAndView("admin/account");
+        List<Account> accounts = adminAccountService.getAllAccounts();
+        modelAndView.addObject("accounts", accounts);
+        return modelAndView;
+    }
+
+
 }
